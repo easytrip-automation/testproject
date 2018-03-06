91791a8d4f9bf6f1c5bbd5d58702db4da4d4194f
volodyatrots <vova_lvova@i.ua>
2018-03-06 17:56:35 +0200

    add files

---
+++ src/test/java/com/epam/lab/tests/TestGmail.java
@@ -0,0 +1,32 @@
+package com.epam.lab.tests;
+
+import com.epam.lab.businessobjects.GmailLoginBO;
+import com.epam.lab.driver.Driver;
+import com.epam.lab.models.UserModel;
+import com.epam.lab.utils.provider.TestProvider;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class TestGmail {
+
+    @Test(dataProvider = "getUsersData", dataProviderClass = TestProvider.class)
+    public void sendFromDrafts(UserModel userModel) {
+
+        GmailLoginBO gmailLoginBO = new GmailLoginBO();
+
+        gmailLoginBO.openLoginPage();
+
+        gmailLoginBO.loginAS(userModel);
+
+        assertTrue(gmailLoginBO.checkLoginSuccess());
+
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        Driver.close();
+    }
+}
