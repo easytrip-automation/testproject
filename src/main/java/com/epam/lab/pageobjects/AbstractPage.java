d53ef8eccbcfa919b07ac8751eb72cb7e9600f67
olenkayurkivm <olenayurkiv2017@gmail.com>
2018-04-04 12:58:35 +0300

    Abstract page Waits

--- src/main/java/com/epam/lab/pageobjects/AbstractPage.java
+++ src/main/java/com/epam/lab/pageobjects/AbstractPage.java
@@ -2,10 +2,45 @@ package com.epam.lab.pageobjects;
 
 
 import com.epam.lab.driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract class AbstractPage {
-    AbstractPage() {
-        PageFactory.initElements(DriverFactory.getDriver(), this);
+    private WebDriver driver;
+
+    AbstractPage( ) {
+        PageFactory.initElements( driver = DriverFactory.getDriver(), this);
     }
+
+    private WebDriverWait getWait() {
+        return (new WebDriverWait(driver, 30));
+    }
+
+    void openPage(String pageURL) {
+        driver.get(pageURL);
+    }
+
+    String  getTitle (){
+        return driver.getTitle();
+    }
+
+    void waitUntilTitleIs(String title ) {
+        getWait().until(ExpectedConditions.titleIs(title));
+    }
+
+    void waitUntilTitleContains(String title ) {
+        getWait().until(ExpectedConditions.titleContains(title));
+    }
+
+    void waitUntilClickable(WebElement element) {
+        getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    void waitUntilVisible(WebElement element) {
+        getWait().until(ExpectedConditions.visibilityOf(element));
+    }
+
 }
