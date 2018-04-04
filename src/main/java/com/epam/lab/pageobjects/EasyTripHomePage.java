6d2f0d35c53de8fefcbb573e1ac332f76513471a
Admin <anjarusnak19@gmail.com>
2018-04-04 12:38:05 +0300

    minor changes in HomePage

--- src/main/java/com/epam/lab/pageobjects/EasyTripHomePage.java
+++ src/main/java/com/epam/lab/pageobjects/EasyTripHomePage.java
@@ -18,134 +18,122 @@ public class EasyTripHomePage extends AbstractPage {
     @FindBy(xpath = "//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
     private WebElement signInButton;
 
-    public void clickSignInButton() {
-        signInButton.click();
-    }
-
     @FindBy(xpath = "//*[@id=\"exampleInputPassword1\"]")
     private WebElement userNameInput;
-
-    public void enterUserName(String username) {
-        userNameInput.sendKeys(username);
-    }
 
     @FindBy(xpath = "//*[@id=\"exampleInputPassword1\"]")
     private WebElement passwordInputField;
 
-    public void enterPasword(String pass) {
-        passwordInputField.sendKeys(pass);
-    }
-
     @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-login/div[2]/div/div[2]/form/fieldset/button")
     private WebElement submitButton;
-
-    public void clickSubmitButton() {
-        submitButton.click();
-    }
 
     @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-login/div[3]/a[1]")
     private WebElement signUpButton;
 
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-login/div[3]/a[2]")
+    private WebElement forgotPasswordButton;
+
+    @FindBy(xpath = "//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
+    private WebElement yourProfileButton;
+
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[1]/button/span")
+    private WebElement closeProfileButton;
+
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[3]/div/a")
+    private WebElement manageProfileButton;
+
+    @FindBy(xpath = "//*[@id=\"firstName\"]")
+    private WebElement firstNameField;
+
+    @FindBy(xpath = "//*[@id=\"lastName\"]")
+    private WebElement lastNameField;
+
+    @FindBy(xpath = "//*[@id=\"email\"]")
+    private WebElement emailField;
+
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[2]/div/div/form/button")
+    private WebElement updateButton;
+
+
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[2]/div/div/form/a")
+    private WebElement changePasswordButton;
+
+    @FindBy(xpath = "//*[@id=\"oldPassword\"]")
+    private WebElement oldPasswordInput;
+
+    @FindBy(xpath = "//*[@id=\"newPassword\"]")
+    private WebElement newPasswordInput;
+
+    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-change-password/div[3]/div/a")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a")
+    private WebElement logoutButton;
+
+    @FindBy(xpath = "//*[@id=\"carouselExampleCaptions\"]/div/div[1]/div/app-planning-form/a")
+    private WebElement planYourTripButton;
+
+    public void clickSignInButton() {
+        signInButton.click();
+    }
+    public void enterUserName(String username) {
+        userNameInput.sendKeys(username);
+    }
+    public void enterPasword(String pass) {
+        passwordInputField.sendKeys(pass);
+    }
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
     public void clickSignUp() {
         signUpButton.click();
     }
-
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-login/div[3]/a[2]")
-    private WebElement forgotPasswordButton;
 
     public void clickForgotPasswordButton() {
         forgotPasswordButton.click();
     }
 
-    @FindBy(xpath = "//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
-    private WebElement yourProfileButton;
-
     public void goToProfile() {
         yourProfileButton.click();
     }
-
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[1]/button/span")
-    private WebElement closeProfileButton;
 
     public void clickCloseProfileButton() {
         closeProfileButton.click();
     }
 
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[3]/div/a")
-    private WebElement manageProfileButton;
-
     public void clickManageProfileButton() {
         manageProfileButton.click();
     }
-
-    @FindBy(xpath = "//*[@id=\"firstName\"]")
-    private WebElement firstNameField;
-
     public void editUserName(String newName) {
         firstNameField.clear();
         firstNameField.sendKeys(newName);
     }
-
-    @FindBy(xpath = "//*[@id=\"lastName\"]")
-    private WebElement lastNameField;
-
     public void editLastName(String newName) {
         lastNameField.clear();
         lastNameField.sendKeys(newName);
     }
-
-    @FindBy(xpath = "//*[@id=\"email\"]")
-    private WebElement emailField;
-
     public void editEmail(String newEmail) {
         emailField.clear();
         emailField.sendKeys(newEmail);
     }
-
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[2]/div/div/form/button")
-    private WebElement updateButton;
-
     public void saveChanges() {
         updateButton.click();
     }
-
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-profile/div[2]/div/div/form/a")
-    private WebElement changePasswordButton;
-
     public void clickChangePasswordButton() {
         changePasswordButton.click();
     }
-
-    @FindBy(xpath = "//*[@id=\"oldPassword\"]")
-    private WebElement oldPasswordInput;
-
     public void enterOldPassword(String oldPass) {
         oldPasswordInput.sendKeys(oldPass);
     }
-
-    @FindBy(xpath = "//*[@id=\"newPassword\"]")
-    private WebElement newPasswordInput;
-
     public void enterNewPassword(String newPass) {
         newPasswordInput.sendKeys(newPass);
     }
-
-    @FindBy(xpath = "/html/body/ngb-modal-window/div/div/app-change-password/div[3]/div/a")
-    private WebElement saveButton;
-
     public void saveChangedPassword() {
         saveButton.click();
     }
-
-    @FindBy(xpath = "//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a")
-    private WebElement logoutButton;
-
     public void clickLogoutButton() {
         logoutButton.click();
     }
-    @FindBy(xpath = "//*[@id=\"carouselExampleCaptions\"]/div/div[1]/div/app-planning-form/a")
-    private WebElement planYourTripButton;
-
     public void clickPlanYourTripButton(){
         planYourTripButton.click();
     }
