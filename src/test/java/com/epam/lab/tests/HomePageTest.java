ad50e186a84615c602b2fb821193ade212044dcf
Admin <anjarusnak19@gmail.com>
2018-04-04 15:50:52 +0300

    HomePageTest class created +login with empty field test

---
+++ src/test/java/com/epam/lab/tests/HomePageTest.java
@@ -0,0 +1,29 @@
+package com.epam.lab.tests;
+
+import com.epam.lab.businesobjects.HomePageFacade;
+import com.epam.lab.driver.DriverFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+    public HomePageFacade homePageFacade;
+
+    @BeforeTest
+    public void setUp() {
+        homePageFacade = new HomePageFacade();
+        homePageFacade.openHomePage();
+    }
+
+    @Test
+    public void loginWithEmptyFieldTest() {
+        homePageFacade.loginWithNoData("", "");
+        Assert.assertTrue(homePageFacade.isPassFieldPresent());
+    }
+    
+    @AfterTest
+    public void tearDown(){
+        DriverFactory.close();
+    }
+}
